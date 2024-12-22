b6fa89938828f278866b325239e6a38e2ef7e2ca
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2024-12-22 21:34:06 +0200

    Nullable Value

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -24,6 +24,67 @@
             // UnSafe Casting [May Throw Exception]
 
             #endregion
+            #region Nullable Value
+
+            //Nullable Type 
+            //Value Type 
+            //Reference Type
+
+            //int Num = 5;
+            //Num = null; //Invalid
+            //Nullable <int>: Allow Null
+            //int? Num = 5;
+            //Num = null;
+
+            //double? Salary = 4000.5;
+            //Salary = null;
+
+            //int x = 5;
+            //int? y = x;
+
+
+            int? x;
+            x = null;
+
+            int y;
+
+            //UnBoxing 
+            //May Throw Exeption
+
+
+            //Defensive Code
+            //if(x != null)
+            //{
+            //    y = (int)x;
+
+            //}
+            //else
+            //{
+            //    y = 0;
+            //}
+
+            //==============================
+            //if (x is not null)
+            //{
+            //    y = (int)x;
+
+            //}
+            //else
+            //{
+            //    y = 0;
+            //}
+
+            //if (x.HasValue)
+            //    y = x.Value;
+            //else
+            //    y = 0;
+            //y = x.HasValue ? x.Value : 0;
+            //y = x is not null ? x.Value : 0;
+
+            //===========================
+
+            /*y = x ?? 0; *///Syntax Sugar :x is not null ? x.Value : 0; 
+            #endregion
         }
     }
 }
