da267acba2aae96d542b80350b72d6d4cc6cbef6
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2024-12-22 20:51:23 +0200

    Boxing&UnBoxing

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Boxing&UnBoxing
+            //Boxing : Casting From Value Type To References Value 
+            //UnBoxing : Casting From References Type To References Value Type
+
+            //object O1;
+            ////O1 => Refer To Instance Of 'Object' Or Any Instance Of Any Type 
+            ////O1 = "khaled";
+            ////O1 = 10;
+            ////O1 = 3; 
+            //int Num = 5;
+            //O1 =(object) Num; //Boxing Impliced Casting Safe
+
+
+            //object O1 = 3;
+            //int Num = (int) O1; 
+            //UnBoxing
+            //Explicit Casting
+            // UnSafe Casting [May Throw Exception]
+
+            #endregion
         }
     }
 }
